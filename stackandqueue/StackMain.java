0e5ab748b70a876e89d89524bf8146a05b9dd6e0
JVignesh4 <vigneshbuji04@gmail.com>
2022-07-09 13:37:30 +0530

    Added: Stack And Queue Programe

---
+++ stackandqueue/StackMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.stackandqueue;
+
+
+
+public class StackMain {
+    public static void main(String[] args) {
+        System.out.println("----- Welcome To Data Structure : Stack -----");
+    }
+
+}
