785ceaa0c5ea016f8b3d93134de6fb5423b9e05f
JVignesh4 <vigneshbuji04@gmail.com>
2022-07-09 13:40:36 +0530

    Added: Created New Stack List

--- stackandqueue/Stack.java
+++ stackandqueue/Stack.java
@@ -1,5 +1,24 @@
 package com.bridgelabz.stackandqueue;
 
+import com.blz.linkedlist.INode;
+import com.blz.linkedlist.LinkList;
+import com.blz.linkedlist.MyNode;
 
 public class Stack<K> {
+    private final LinkList stackLinkedList;
+
+    public Stack() {
+        stackLinkedList = new LinkList();
+    }
+
+
+    public void push(INode element) {
+        stackLinkedList.addNode((MyNode) element);
+    }
+
+
+    public void printStack() {
+        stackLinkedList.display();
+    }
+
 }
--- stackandqueue/StackMain.java
+++ stackandqueue/StackMain.java
@@ -1,10 +1,20 @@
 package com.bridgelabz.stackandqueue;
 
-
+import com.blz.linkedlist.MyNode;
 
 public class StackMain {
     public static void main(String[] args) {
         System.out.println("----- Welcome To Data Structure : Stack -----");
+
+        Stack stackList = new Stack();
+        MyNode<Integer> firstNode = new MyNode<Integer>(70);
+        MyNode<Integer> secondNode = new MyNode<Integer>(30);
+        MyNode<Integer> thirdNode = new MyNode<Integer>(56);
+        stackList.push(firstNode);
+        stackList.push(secondNode);
+        stackList.push(thirdNode);
+        stackList.printStack();
+
     }
 
 }
